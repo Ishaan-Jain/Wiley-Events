65eab6414d2985b852f401218b1a8cd0785017de
Ishaan Jain <ishaanj@Ishaans-MacBook-Air.local>
2022-08-31 11:05:57 -0400

    Deployment changes

--- config/db.js
+++ config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
   try{
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    // const conn = await mongoose.connect("mongodb+srv://User:masterchef@cluster0.zywcanw.mongodb.net/?retryWrites=true&w=majority"
-    // );
     console.log(`MongoDB connected: ${conn.connection.host}`)
   }catch(error){
     console.log(error);
